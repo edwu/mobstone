83e330436ff997c079cc771101c3a294389c260e
Edward Wu <edwardwu4@gmail.com>
2011-10-29 16:59:54 -0700

    Initial commit for ieor191 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobstone::Application.config.secret_token = '1bb0079455e959efbe4b8c6bcf65dbee4f974e68837eea79a9e5c51c9de14b01a63800283efa7e57e1397a3c4a8c84c44adb65e959fa0dcb40cd52cd43b70858'
