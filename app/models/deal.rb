d33afb9c3d88cbc6434eeb722028eacbc2dbd9f1
Connie Guo <connie.guo107@gmail.com>
2011-11-09 00:13:30 -0800

    added company purchases page

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -7,5 +7,32 @@ class Deal < ActiveRecord::Base
   accepts_nested_attributes_for :milestones
   
   mount_uploader :image, ImageUploader
+  
+  def current_price
+    number_bought = self.vouchers.count
+    sorted_milestones = self.milestones.sort{|a,b| b.people <=> a.people}
+    
+    previous_milestone = sorted_milestones[0]
+  
+    
+    sorted_milestones.each do |milestone|
+      if number_bought > milestone.people
+        previous_milestone = milestone
+        end        
+    end
+    
+    return previous_milestone.price
+       
+  end
+  
+  def percent_reached
+    number_bought = self.vouchers.count
+    puts "number_bought:#{number_bought}"
+    milestones = self.milestones
+    max_milestone = milestones.maximum("people")
+    puts "max_milestone:#{max_milestone}"
+    
+    return number_bought / (max_milestone*1.0)
+  end
 
 end
