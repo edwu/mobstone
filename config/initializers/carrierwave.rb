0e4a4674d1f31f38d158ae1260c3beb425438c64
Edward Wu <edwardwu4@gmail.com>
2011-11-15 22:46:50 -0800

    carrierwave fix?

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,7 @@ CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp')
   config.cache_dir = 'carrierwave'
 
-  config.s3_access_key_id = ENV['AKIAJCTGNYPQ2HD2KUGA']
-  config.s3_secret_access_key = ENV['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']
-  config.s3_bucket = ENV['mobstone']
+  config.s3_access_key_id = ENV['s3_access_key_id']
+  config.s3_secret_access_key = ENV['s3_secret_access_key']
+  config.s3_bucket = ENV['s3_bucket']
 end
-
